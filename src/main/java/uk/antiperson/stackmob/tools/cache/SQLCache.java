b454690298dfaf3606bfa2fc162fe1723a980a4b
BuildTools <nathat890@outlook.com>
2017-12-28 11:12:30 +0000

    Completed SQL caching (#20)

--- src/main/java/uk/antiperson/stackmob/tools/cache/SQLCache.java
+++ src/main/java/uk/antiperson/stackmob/tools/cache/SQLCache.java
@@ -3,28 +3,34 @@ package uk.antiperson.stackmob.tools.cache;
 import uk.antiperson.stackmob.StackMob;
 
 import java.sql.*;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 // Template for future SQL caching.
 
-public class SQLCache implements Cache {
+public class SQLCache implements Cache{
 
     private String username;
     private String password;
     private String finalUrl;
+    private Connection con;
+    private StackMob sm;
     public SQLCache(StackMob sm){
         String serverUrl = sm.config.getCustomConfig().getString("caching.mysql.server-ip");
         int serverPort = sm.config.getCustomConfig().getInt("caching.mysql.server-port");
         username = sm.config.getCustomConfig().getString("caching.mysql.username");
         password = sm.config.getCustomConfig().getString("caching.mysql.password");
         finalUrl = "jdbc:mysql://" + serverUrl + ":" + serverPort + "/stackmob?autoReconnect=true&useSSL=false";
+        this.sm = sm;
     }
 
 
     public int read(UUID uuid){
         try{
-            return performCommandResults("SELECT * FROM CACHE WHERE UUID='" + uuid.toString() + "';").getInt(0);
+            ResultSet rs = con.createStatement().executeQuery("SELECT Size FROM CACHE WHERE UUID='" + uuid.toString() + "';");
+            rs.next();
+            return rs.getInt(1);
         }catch (SQLException e){
             e.printStackTrace();
         }
@@ -33,49 +39,66 @@ public class SQLCache implements Cache {
 
     public void write(UUID uuid, int value){
         remove(uuid);
-        performCommand("INSERT INTO CACHE VALUES ('" + uuid.toString() + "', " + value + ");");
+        try {
+            con.createStatement().execute("INSERT INTO CACHE VALUES ('" + uuid.toString() + "', " + value + ");");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public boolean contains(UUID uuid){
+        try {
+            return con.createStatement().executeQuery("SELECT UUID FROM CACHE WHERE UUID='" + uuid.toString()+ "';").next();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return false;
     }
 
     public void remove(UUID uuid){
-        performCommand("DELETE FROM CACHE WHERE UUID='" + uuid.toString() + "';");
+        try {
+            con.createStatement().execute("DELETE FROM CACHE WHERE UUID='" + uuid.toString() + "';");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void load(){
-        performCommand("CREATE TABLE IF NOT EXISTS CACHE (UUID varchar(255), Size int);");
+        try{
+            inialize();
+            con.createStatement().execute("CREATE TABLE IF NOT EXISTS CACHE (UUID varchar(255), Size int);");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void close(){
-
-    }
-
-    public Set<UUID> getKeys(){
-        return null;
-    }
-
-    public CacheType getType() {
-        return CacheType.SQL;
-    }
-
-    public void performCommand(String command){
-        try{
-            Connection con = DriverManager.getConnection(finalUrl, username, password);
-            Statement statement = con.createStatement();
-            statement.execute(command);
+        try {
             con.close();
         }catch (SQLException e){
             e.printStackTrace();
         }
     }
 
-    public ResultSet performCommandResults(String command) throws SQLException{
-        Connection con = DriverManager.getConnection(finalUrl, username, password);
-        Statement statement = con.createStatement();
-        ResultSet rs = statement.executeQuery(command);
-        con.close();
-        return rs;
+    public Set<UUID> getKeys(){
+        HashSet<UUID> keys = new HashSet<>();
+        try{
+            ResultSet rs = con.createStatement().executeQuery("SELECT UUID FROM CACHE;");
+            while (rs.next()){
+                keys.add(UUID.fromString(rs.getString(1)));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return keys;
+    }
+
+    public CacheType getType() {
+        return CacheType.SQL;
+    }
+
+    public void inialize() throws SQLException{
+        con = DriverManager.getConnection(finalUrl, username, password);
     }
 }
