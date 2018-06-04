5461006a5aa32c0d785b39dd7ff626fc4ee599cf
nathat890 <nathat890@outlook.com>
2018-06-04 20:45:53 +0100

    Fixes #60 - Kill-all and kill-step cannot be used at same time.

--- src/main/java/uk/antiperson/stackmob/listeners/entity/DeathEvent.java
+++ src/main/java/uk/antiperson/stackmob/listeners/entity/DeathEvent.java
@@ -26,7 +26,7 @@ public class DeathEvent implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onDeath(EntityDeathEvent e) {
-        Entity dead = e.getEntity();
+        LivingEntity dead = e.getEntity();
 
         if(!dead.hasMetadata(GlobalValues.METATAG)){
             return;
@@ -46,11 +46,13 @@ public class DeathEvent implements Listener {
                     if (!sm.config.getCustomConfig().getStringList("kill-all.type-blacklist")
                             .contains(dead.getType().toString())) {
                         // Do it
-                        multiplication(e.getEntity(), e.getDrops(), oldSize - 1, e.getDroppedExp());
-                        subtractAmount = oldSize;
+                        multiplication(dead, e.getDrops(), oldSize - 1, e.getDroppedExp());
+                        finished(oldSize, oldSize, dead);
+                        return;
                     }
                 }
-            }else if(sm.config.getCustomConfig().getBoolean("kill-step.enabled")){
+            }
+            if(sm.config.getCustomConfig().getBoolean("kill-step.enabled")){
                 if (!sm.config.getCustomConfig().getStringList("kill-step.reason-blacklist")
                         .contains(dead.getLastDamageCause().getCause().toString())) {
                     if (!sm.config.getCustomConfig().getStringList("kill-step.type-blacklist")
@@ -61,26 +63,15 @@ public class DeathEvent implements Listener {
                         }else{
                             subtractAmount = randomStep;
                         }
-                        multiplication(e.getEntity(), e.getDrops(), subtractAmount - 1, e.getDroppedExp());
+                        multiplication(dead, e.getDrops(), subtractAmount - 1, e.getDroppedExp());
                     }
                 }
             }
         }
-
-        if(oldSize != subtractAmount){
-            Entity newe = sm.tools.duplicate(dead);
-            newe.setMetadata(GlobalValues.METATAG, new FixedMetadataValue(sm, oldSize - subtractAmount));
-            newe.setMetadata(GlobalValues.NO_SPAWN_STACK, new FixedMetadataValue(sm, true));
-        }
-        dead.removeMetadata(GlobalValues.METATAG, sm);
-        dead.removeMetadata(GlobalValues.NO_STACK_ALL, sm);
-        dead.removeMetadata(GlobalValues.NO_TASK_STACK, sm);
-        dead.removeMetadata(GlobalValues.CURRENTLY_BREEDING, sm);
-        dead.removeMetadata(GlobalValues.NOT_ENOUGH_NEAR, sm);
-        dead.removeMetadata(GlobalValues.KILL_ONE_OFF, sm);
+        finished(oldSize, subtractAmount, dead);
     }
 
-    public void multiplication(LivingEntity dead, List<ItemStack> drops, int subtractAmount, int originalExperience){
+    private void multiplication(LivingEntity dead, List<ItemStack> drops, int subtractAmount, int originalExperience){
         if(sm.config.getCustomConfig().getBoolean("multiply-drops.enabled")){
             if(dead.getKiller() != null){
                 sm.dropTools.calculateDrops(drops, subtractAmount, dead.getLocation(), dead.getKiller().getItemInHand());
@@ -92,5 +83,19 @@ public class DeathEvent implements Listener {
             int newExperience = (int) Math.round(originalExperience * (subtractAmount - 1) * ThreadLocalRandom.current().nextDouble(0.5));
             ((ExperienceOrb) dead.getWorld().spawnEntity(dead.getLocation(), EntityType.EXPERIENCE_ORB)).setExperience(newExperience);
         }
+    }
+
+    private void finished(int oldSize, int subtractAmount, Entity dead){
+        if(oldSize != subtractAmount){
+            Entity newe = sm.tools.duplicate(dead);
+            newe.setMetadata(GlobalValues.METATAG, new FixedMetadataValue(sm, oldSize - subtractAmount));
+            newe.setMetadata(GlobalValues.NO_SPAWN_STACK, new FixedMetadataValue(sm, true));
+        }
+        dead.removeMetadata(GlobalValues.METATAG, sm);
+        dead.removeMetadata(GlobalValues.NO_STACK_ALL, sm);
+        dead.removeMetadata(GlobalValues.NO_TASK_STACK, sm);
+        dead.removeMetadata(GlobalValues.CURRENTLY_BREEDING, sm);
+        dead.removeMetadata(GlobalValues.NOT_ENOUGH_NEAR, sm);
+        dead.removeMetadata(GlobalValues.KILL_ONE_OFF, sm);
     }
 }
