4fcbafd1c131510fd6227c5d30050b6e5a1dc890
nathat890 <nathat890@outlook.com>
2018-07-28 21:54:53 +0100

    Fix for #75

--- src/main/java/uk/antiperson/stackmob/tools/EntityTools.java
+++ src/main/java/uk/antiperson/stackmob/tools/EntityTools.java
@@ -182,7 +182,7 @@ public class EntityTools {
         Location dupeLoc;
         if(sm.pluginSupport.getMythicSupport() != null && sm.pluginSupport.getMythicSupport().isMythicMob(original)){
             dupe = sm.pluginSupport.getMythicSupport().spawnMythicMob(original);
-        }else if (original instanceof Zombie){
+        }else if (original instanceof Zombie || original instanceof Skeleton){
         	// will spawn the dupe in the middle of the block the original died in, prevents mobs from glitching through walls due to "safe spawn errors"
         	dupeLoc = new Location(original.getWorld(), original.getLocation().getBlockX()+0.5, original.getLocation().getY(), original.getLocation().getBlockZ()+0.5);
             dupe = original.getWorld().spawnEntity(dupeLoc, original.getType());
