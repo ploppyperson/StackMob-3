9883f2aec362bf6197adfd5851c63f7048affb66
nathat890 <nathat890@outlook.com>
2018-08-09 18:06:00 +0100

    Add support for WorldGuard 7.0, resolves #93

--- src/main/java/uk/antiperson/stackmob/tools/plugin/WorldGuardSupport.java
+++ src/main/java/uk/antiperson/stackmob/tools/plugin/WorldGuardSupport.java
@@ -1,17 +1,17 @@
 package uk.antiperson.stackmob.tools.plugin;
 
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
 import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
-import org.bukkit.Bukkit;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
 import org.bukkit.Location;
 import uk.antiperson.stackmob.StackMob;
 
 public class WorldGuardSupport {
 
-    private WorldGuardPlugin wgp = (WorldGuardPlugin) Bukkit.getServer().getPluginManager().getPlugin("WorldGuard");
     private static final StateFlag ENTITY_FLAG = new StateFlag("entity-stacking", true);
     private StackMob sm;
     public WorldGuardSupport(StackMob sm){
@@ -19,7 +19,7 @@ public class WorldGuardSupport {
     }
 
     public void registerFlag(){
-        FlagRegistry registry = wgp.getFlagRegistry();
+        FlagRegistry registry = WorldGuard.getInstance().getFlagRegistry();
         try{
             registry.register(ENTITY_FLAG);
         }catch (FlagConflictException e){
@@ -29,11 +29,13 @@ public class WorldGuardSupport {
     }
 
     public boolean checkCanStack(Location location){
-        ApplicableRegionSet ars = wgp.getRegionManager(location.getWorld()).getApplicableRegions(location);
-        return ars.testState(null, ENTITY_FLAG);
+        try {
+            RegionContainer rc = WorldGuard.getInstance().getPlatform().getRegionContainer();
+            ApplicableRegionSet ars = rc.get(BukkitAdapter.adapt(location.getWorld())).getApplicableRegions(BukkitAdapter.adapt(location).toVector());
+            return ars.testState(null, ENTITY_FLAG);
+        }catch (NullPointerException e){
+            return false;
+        }
     }
 
-    public WorldGuardPlugin getWgp() {
-        return wgp;
-    }
 }
