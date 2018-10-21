6504ac3af0272053a17e82aaa3e65083fd1f71c3
nathat890 <nathat890@outlook.com>
2018-10-21 11:55:49 +0100

    Remove unused method.

--- src/main/java/uk/antiperson/stackmob/tasks/StackingTask.java
+++ src/main/java/uk/antiperson/stackmob/tasks/StackingTask.java
@@ -8,14 +8,12 @@ public abstract class StackingTask extends BukkitRunnable {
     private double xRadius;
     private double yRadius;
     private double zRadius;
-    private int maxStackSize;
     private StackMob sm;
     public StackingTask(StackMob sm){
         this.sm = sm;
         xRadius = sm.getCustomConfig().getDouble("check-area.x");
         yRadius = sm.getCustomConfig().getDouble("check-area.y");
         zRadius = sm.getCustomConfig().getDouble("check-area.z");
-        maxStackSize = sm.getCustomConfig().getInt("stack-max");
     }
 
     public double getX() {
@@ -28,10 +26,6 @@ public abstract class StackingTask extends BukkitRunnable {
 
     public double getZ(){
         return zRadius;
-    }
-
-    public int getMaxStackSize() {
-        return maxStackSize;
     }
 
     public StackMob getStackMob() {
