69979fc275305de228b47d84f323ba496662d8cf
nathat890 <nathat890@outlook.com>
2018-11-03 13:46:47 +0000

    Add check for drowned only.

--- src/main/java/uk/antiperson/stackmob/listeners/entity/ConvertEvent.java
+++ src/main/java/uk/antiperson/stackmob/listeners/entity/ConvertEvent.java
@@ -3,7 +3,9 @@ package uk.antiperson.stackmob.listeners.entity;
 import com.destroystokyo.paper.event.entity.EntityTransformedEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.metadata.FixedMetadataValue;
 import uk.antiperson.stackmob.StackMob;
+import uk.antiperson.stackmob.tools.extras.GlobalValues;
 
 public class ConvertEvent implements Listener {
 
@@ -14,9 +16,14 @@ public class ConvertEvent implements Listener {
 
     @EventHandler
     public void onStuff(EntityTransformedEvent event){
+        if(event.getReason() != EntityTransformedEvent.TransformedReason.DROWNED){
+            return;
+        }
         if(sm.getStackTools().hasValidStackData(event.getEntity())){
             int stackSize = sm.getStackTools().getSize(event.getEntity());
+
             sm.getStackTools().setSize(event.getTransformed(), stackSize - 1);
+            event.getTransformed().setMetadata(GlobalValues.NO_SPAWN_STACK, new FixedMetadataValue(sm, true));
 
             sm.getStackTools().removeSize(event.getEntity());
         }
