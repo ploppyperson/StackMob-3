f52189bc6d1d3d47e9da81cdca70cbfacadc7ae2
nathat890 <nathat890@outlook.com>
2018-12-01 21:56:02 +0000

    Get rid of unneeded super class.

--- src/main/java/uk/antiperson/stackmob/tasks/SpawnTask.java
+++ src/main/java/uk/antiperson/stackmob/tasks/SpawnTask.java
@@ -1,15 +1,17 @@
 package uk.antiperson.stackmob.tasks;
 
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.scheduler.BukkitRunnable;
 import uk.antiperson.stackmob.StackMob;
 import uk.antiperson.stackmob.entity.StackTools;
 import uk.antiperson.stackmob.tools.extras.GlobalValues;
 
-public class SpawnTask extends StackingTask {
+public class SpawnTask extends BukkitRunnable {
 
     private LivingEntity entity;
+    private StackMob sm;
     public SpawnTask(StackMob sm, LivingEntity entity){
-        super(sm);
+        this.sm = sm;
         this.entity = entity;
     }
 
@@ -21,18 +23,18 @@ public class SpawnTask extends StackingTask {
         // Set metadata to we can stack.
         StackTools.setSize(entity,1);
         // Find nearby stacks to merge with, return if found.
-        if(getStackMob().getLogic().foundMatch(entity)){
+        if(sm.getLogic().foundMatch(entity)){
             return;
         }
 
         // A match was not found, so we will set the appropriate metadata.
-        if(getStackMob().getCustomConfig().getInt("dont-stack-until") > 0){
-            if(getStackMob().getLogic().notEnoughNearby(entity)){
+        if(sm.getCustomConfig().getInt("dont-stack-until") > 0){
+            if(sm.getLogic().notEnoughNearby(entity)){
                 StackTools.setSize(entity, GlobalValues.NOT_ENOUGH_NEAR);
             }
         }
 
         // Set traits.
-        getStackMob().getTools().setTraits(entity);
+        sm.getTools().setTraits(entity);
     }
 }
--- src/main/java/uk/antiperson/stackmob/tasks/StackTask.java
+++ src/main/java/uk/antiperson/stackmob/tasks/StackTask.java
@@ -4,14 +4,16 @@ import org.bukkit.World;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 import uk.antiperson.stackmob.StackMob;
 import uk.antiperson.stackmob.entity.StackTools;
 
-public class StackTask extends StackingTask {
+public class StackTask extends BukkitRunnable {
 
     private World world;
+    private StackMob sm;
     public StackTask(StackMob sm, World world){
-        super(sm);
+        this.sm = sm;
         this.world = world;
     }
 
@@ -22,14 +24,14 @@ public class StackTask extends StackingTask {
                 continue;
             }
             if(StackTools.hasNotEnoughNear(entity)) {
-                if(getStackMob().getLogic().notEnoughNearby(entity)){
+                if(sm.getLogic().notEnoughNearby(entity)){
                     continue;
                 }
             }
-            if(getStackMob().getLogic().notSuitableForStacking(entity)){
+            if(sm.getLogic().notSuitableForStacking(entity)){
                 continue;
             }
-            getStackMob().getLogic().foundMatch(entity);
+            sm.getLogic().foundMatch(entity);
         }
     }
 }
