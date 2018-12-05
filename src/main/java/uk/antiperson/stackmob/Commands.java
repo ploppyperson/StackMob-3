ca2974ea5ade5a9de59e5bf47a874c2b3beb43b2
BuildTools <javw.phanatic@gmail.com>
2018-12-05 07:01:26 -0500

    make sure all metadata is removed on entity removal

--- src/main/java/uk/antiperson/stackmob/Commands.java
+++ src/main/java/uk/antiperson/stackmob/Commands.java
@@ -63,6 +63,14 @@ public class Commands implements CommandExecutor {
                             if (entity.hasMetadata(GlobalValues.METATAG)) {
                                 counter++;
                                 entity.remove();
+                                entity.removeMetadata(GlobalValues.METATAG, sm);
+                                entity.removeMetadata(GlobalValues.NO_STACK_ALL, sm);
+                                entity.removeMetadata(GlobalValues.NO_TASK_STACK, sm);
+                                entity.removeMetadata(GlobalValues.CURRENTLY_BREEDING, sm);
+                                entity.removeMetadata(GlobalValues.NOT_ENOUGH_NEAR, sm);
+                                entity.removeMetadata(GlobalValues.KILL_ONE_OFF, sm);
+                                entity.removeMetadata(GlobalValues.NO_SPAWN_STACK, sm);
+                                entity.removeMetadata(GlobalValues.MCMMO_META, sm);
                             }
                         }
                     }
@@ -119,6 +127,14 @@ public class Commands implements CommandExecutor {
                             for (Entity entity : ((Player) sender).getNearbyEntities(numb, numb, numb)) {
                                 if (entity.hasMetadata(GlobalValues.METATAG)) {
                                     entity.remove();
+                                    entity.removeMetadata(GlobalValues.METATAG, sm);
+                                    entity.removeMetadata(GlobalValues.NO_STACK_ALL, sm);
+                                    entity.removeMetadata(GlobalValues.NO_TASK_STACK, sm);
+                                    entity.removeMetadata(GlobalValues.CURRENTLY_BREEDING, sm);
+                                    entity.removeMetadata(GlobalValues.NOT_ENOUGH_NEAR, sm);
+                                    entity.removeMetadata(GlobalValues.KILL_ONE_OFF, sm);
+                                    entity.removeMetadata(GlobalValues.NO_SPAWN_STACK, sm);
+                                    entity.removeMetadata(GlobalValues.MCMMO_META, sm);
                                     counter++;
                                 }
                             }
