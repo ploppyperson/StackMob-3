b578ac4f3f39127803f09b5e27dfec74f6db9d31
Stealth2800 <stealth2800@stealthyone.com>
2018-12-29 01:08:52 -0800

    Improve MySQL backend

---
+++ src/main/java/uk/antiperson/stackmob/tools/UuidUtil.java
@@ -0,0 +1,17 @@
+package uk.antiperson.stackmob.tools;
+
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+public final class UuidUtil {
+
+    private static final Pattern UUID_PATTERN = Pattern.compile("(\\w{8})-?(\\w{4})-?(\\w{4})-?(\\w{4})-?(\\w{12})");
+
+    private UuidUtil() {
+    }
+
+    public static UUID fromString(String raw) {
+        return UUID.fromString(UUID_PATTERN.matcher(raw).replaceAll("$1-$2-$3-$4-$5"));
+    }
+
+}
