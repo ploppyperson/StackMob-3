b2f9a157ab44ecad5793d16586373a7296f02b9b
nathat890 <nathat890@outlook.com>
2019-03-31 14:58:09 +0100

    Fixes shears not breaking and provides an error message when not enough durability, still allowing to shear singularly however (#204)

--- src/main/java/uk/antiperson/stackmob/listeners/entity/ShearEvent.java
+++ src/main/java/uk/antiperson/stackmob/listeners/entity/ShearEvent.java
@@ -1,6 +1,10 @@
 package uk.antiperson.stackmob.listeners.entity;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.Tag;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
@@ -36,7 +40,7 @@ public class ShearEvent implements Listener {
         int stackSize = StackTools.getSize(oldEntity);
         if(oldEntity instanceof Sheep){
             Sheep oldSheep = (Sheep) oldEntity;
-            if(sm.getCustomConfig().getBoolean("multiply.sheep-wool")){
+            if(sm.getCustomConfig().getBoolean("multiply.sheep-wool") && hasEnoughDurability(event.getPlayer(), stackSize)){
                 LootContext lootContext = new LootContext.Builder(oldSheep.getLocation()).lootedEntity(oldSheep).build();
                 Collection<ItemStack> loot = oldSheep.getLootTable().populateLoot(new Random(), lootContext);
                 for(ItemStack itemStack : loot){
@@ -56,7 +60,7 @@ public class ShearEvent implements Listener {
         }
 
         if(oldEntity instanceof MushroomCow){
-            if(sm.getCustomConfig().getBoolean("multiply.mooshroom-mushrooms")){
+            if(sm.getCustomConfig().getBoolean("multiply.mooshroom-mushrooms") && hasEnoughDurability(event.getPlayer(), stackSize)){
                 // Duplicate mushrooms
                 ItemStack mushrooms = new ItemStack(Material.RED_MUSHROOM,1);
                 sm.getDropTools().dropDrops(mushrooms, (stackSize - 1) * 5, oldEntity.getLocation());
@@ -79,8 +83,25 @@ public class ShearEvent implements Listener {
     private void damageItemInHand(Player player, int stackSize){
         ItemStack item = player.getInventory().getItemInMainHand();
         Damageable meta = (Damageable) item.getItemMeta();
-        meta.setDamage(meta.getDamage() + stackSize);
-        item.setItemMeta((ItemMeta) meta);
-        player.getInventory().setItemInMainHand(item);
+        int newDamage = meta.getDamage() + stackSize;
+        if(newDamage <= item.getType().getMaxDurability()){
+            meta.setDamage(meta.getDamage() + stackSize);
+            item.setItemMeta((ItemMeta) meta);
+            player.getInventory().setItemInMainHand(item);
+            return;
+        }
+        player.getInventory().setItemInMainHand(null);
+    }
+
+    private boolean hasEnoughDurability(Player player, int stackSize) {
+        ItemStack item = player.getInventory().getItemInMainHand();
+        Damageable meta = (Damageable) item.getItemMeta();
+        int newDamage = meta.getDamage() + stackSize;
+        if(newDamage >= item.getType().getMaxDurability()){
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.RED + "Item is too damaged to shear all!"));
+            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 1, 1);
+            return false;
+        }
+        return true;
     }
 }
