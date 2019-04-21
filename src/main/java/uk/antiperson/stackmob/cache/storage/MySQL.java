0d8ec97957a173c4e20903be6931bfac048ccaf8
Justin Baldwin <justin.d.baldwin91@gmail.com>
2019-04-21 03:55:11 -0400

    Add conversion to binary UUID storage

--- src/main/java/uk/antiperson/stackmob/cache/storage/MySQL.java
+++ src/main/java/uk/antiperson/stackmob/cache/storage/MySQL.java
@@ -37,23 +37,54 @@ public class MySQL extends StackStorage implements DisableCleanup {
         try {
             makeConnection();
             getStackMob().getLogger().info("Database connection successful!");
-            connection.prepareStatement("CREATE TABLE IF NOT EXISTS stackmob (uuid BINARY(16) NOT NULL UNIQUE, size INT NOT NULL, primary key (uuid))").execute();
 
-            try (ResultSet rs = connection.prepareStatement("SELECT HEX(uuid) as uuid, size FROM stackmob").executeQuery()) {
-                while (rs.next()) {
-                    int size = rs.getInt("size");
-                    if (size <= 1) continue;
+            // Convert existing CHAR column UUIDs to BINARY type
+            if(getUUIDStorageType() == 1) {
+                convertToBinaryUUIDStorage();
+            }
+            else {
+                connection.prepareStatement("CREATE TABLE IF NOT EXISTS stackmob (uuid BINARY(16) NOT NULL UNIQUE, size INT NOT NULL, primary key (uuid))").execute();
+                try (ResultSet rs = connection.prepareStatement("SELECT HEX(uuid) as uuid, size FROM stackmob").executeQuery()) {
+                    while (rs.next()) {
+                        int size = rs.getInt("size");
+                        if (size <= 1) continue;
 
-                    getStorageManager().getAmountCache().put(
-                        UuidUtil.fromString(rs.getString("uuid")),
-                        size
-                    );
+                        getStorageManager().getAmountCache().put(
+                                UuidUtil.fromString(rs.getString("uuid")),
+                                size
+                        );
+                    }
                 }
             }
             convert();
         } catch (SQLException e) {
             getStackMob().getLogger().warning("An issue occurred while connecting to the database.");
             getStackMob().getLogger().warning("Please make sure that your database details are correct.");
+            e.printStackTrace();
+        }
+    }
+
+    private int getUUIDStorageType() {
+        try {
+            ResultSet rs = connection.prepareStatement("SELECT * FROM stackmob LIMIT 0").executeQuery();
+            return rs.getMetaData().getColumnType(1);
+        } catch (SQLException e) {
+            return 0;
+        }
+    }
+
+    private void convertToBinaryUUIDStorage() throws SQLException {
+        getStackMob().getLogger().info("Converting existing database to use BINARY type UUIDs");
+        try {
+            ResultSet rs = connection.prepareStatement("SELECT UUID, size FROM stackmob").executeQuery();
+            while (rs.next()){
+                getStorageManager().getAmountCache().put(UUID.fromString(rs.getString(1)), rs.getInt(2));
+            }
+            connection.prepareStatement("DROP TABLE IF EXISTS stackmob").executeQuery();
+            connection.prepareStatement("CREATE TABLE IF NOT EXISTS stackmob (uuid BINARY(16) NOT NULL UNIQUE, size INT NOT NULL, primary key (uuid))").executeQuery();
+            saveStorage(getStorageManager().getAmountCache());
+        }catch (SQLException e){
+            getStackMob().getLogger().warning("An error occurred while converting existing database.");
             e.printStackTrace();
         }
     }
@@ -83,9 +114,11 @@ public class MySQL extends StackStorage implements DisableCleanup {
         closeConnection();
     }
 
-    public void makeConnection() throws SQLException {
-        String url = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?useSSL=false&rewriteBatchedStatements=true";
-        connection = DriverManager.getConnection(url, username, password);
+    private void makeConnection() throws SQLException {
+        if(connection == null || connection.isClosed()) {
+            String url = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?useSSL=false&rewriteBatchedStatements=true";
+            connection = DriverManager.getConnection(url, username, password);
+        }
     }
 
     private void closeConnection() {
