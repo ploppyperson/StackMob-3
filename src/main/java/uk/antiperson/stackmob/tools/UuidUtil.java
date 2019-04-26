26de7d02449c764a399400e19b30380ef3d6985c
nathat890 <nathat890@outlook.com>
2019-04-26 18:04:32 +0100

    remove unneeded class constructor

--- src/main/java/uk/antiperson/stackmob/tools/UuidUtil.java
+++ src/main/java/uk/antiperson/stackmob/tools/UuidUtil.java
@@ -7,9 +7,6 @@ public final class UuidUtil {
 
     private static final Pattern UUID_PATTERN = Pattern.compile("(\\w{8})-?(\\w{4})-?(\\w{4})-?(\\w{4})-?(\\w{12})");
 
-    private UuidUtil() {
-    }
-
     public static UUID fromString(String raw) {
         return UUID.fromString(UUID_PATTERN.matcher(raw).replaceAll("$1-$2-$3-$4-$5"));
     }
