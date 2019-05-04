e7be390d282207f7c51b41bd8521865146a45e2c
nathat890 <nathat890@outlook.com>
2019-05-04 19:50:07 +0100

    fix loading of class.

--- plugin/src/main/java/uk/antiperson/stackmob/bukkit/BukkitHelper.java
+++ plugin/src/main/java/uk/antiperson/stackmob/bukkit/BukkitHelper.java
@@ -7,7 +7,7 @@ public class BukkitHelper {
 
     public static BukkitCompat getBukkitCompat() {
         try {
-            final Class<?> clazz = Class.forName("uk.antiperson.stackmob.api.bukkit." + VersionHelper.getVersion().toString().toLowerCase() + ".BukkitHandler");
+            final Class<?> clazz = Class.forName("uk.antiperson.stackmob.bukkit." + VersionHelper.getVersion().toString().toLowerCase() + ".BukkitHandler");
             // Check if we have a NMSHandler class at that location.
             if (BukkitCompat.class.isAssignableFrom(clazz)) {
                 return  (BukkitCompat) clazz.getConstructor().newInstance();
