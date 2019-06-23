355ab4b2f405782e3fce961b9888cd9f831dde34
nathat890 <nathat890@outlook.com>
2019-06-23 09:22:16 +0100

    Fixes error when defaults are copied (#249)

--- API/src/main/java/uk/antiperson/stackmob/api/config/IConfigLoader.java
+++ API/src/main/java/uk/antiperson/stackmob/api/config/IConfigLoader.java
@@ -3,6 +3,7 @@ package uk.antiperson.stackmob.api.config;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.io.File;
+import java.io.IOException;
 
 public interface IConfigLoader {
     boolean check(String config, String toCheck);
@@ -15,7 +16,7 @@ public interface IConfigLoader {
 
     void generateNewVersion();
 
-    void copyDefault();
+    void copyDefault() throws IOException;
 
     boolean updateConfig();
 }
