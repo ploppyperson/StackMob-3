a5a62c292e95005641506c055330a297f5497967
Aurélien <43724816+Aurelien30000@users.noreply.github.com>
2019-08-21 19:46:30 +0200

    Update ItemDrop.java

--- Plugin/src/main/java/uk/antiperson/stackmob/listeners/entity/ItemDrop.java
+++ Plugin/src/main/java/uk/antiperson/stackmob/listeners/entity/ItemDrop.java
@@ -23,8 +23,6 @@ public class ItemDrop implements Listener {
     @EventHandler
     public void onEggDrop(ItemSpawnEvent event) {
         switch (event.getEntity().getItemStack().getType()) {
-            default:
-                //Nothing to do, without it's considered as a bug by CodeFactor.
             case EGG:
                 // You might be wondering why, but it's to prevent the eggs being multiplied.
                 // Metadata doesn't work because this event is fired before the metadata would be set.
@@ -56,6 +54,9 @@ public class ItemDrop implements Listener {
                         break;
                     }
                 }
+                break;
+            default:
+                //Nothing to do, without it's considered as a bug by CodeFactor.
                 break;
         }
     }
